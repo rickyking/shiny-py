ab07ecf16dedcf5114335def175e8eae97334fca
Yi Jin <y.jin@me.com>
2022-07-31 10:06:46 -0400

    [feat] switch

--- app/app.py
+++ app/app.py
@@ -1,16 +1,29 @@
-from shiny import ui, render, App
 import isodate
+from shiny import App, render, ui
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Note that if the window is narrow, then the sidebar will be shown above the
+# main content, instead of being on the left.
 
 app_ui = ui.page_fluid(
-    ui.input_slider("n", "N", 0, 100, 40),
-    ui.output_text_verbatim("txt"),
+    ui.layout_sidebar(
+        ui.panel_sidebar(ui.input_slider("n", "N", 0, 100, 20)),
+        ui.panel_main(ui.output_plot("plot")),
+    ),
 )
+
 
 def server(input, output, session):
     @output
-    @render.text
-    def txt():
-        return f"n*2 is {input.n() * 2}"
+    @render.plot(alt="A histogram")
+    def plot() -> object:
+        np.random.seed(19680801)
+        x = 100 + 15 * np.random.randn(437)
 
-# This is a shiny.App object. It must be named `app`.
+        fig, ax = plt.subplots()
+        ax.hist(x, input.n(), density=True)
+        return fig
+
+
 app = App(app_ui, server)
